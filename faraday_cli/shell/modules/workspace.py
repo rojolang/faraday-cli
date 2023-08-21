0a64d8b7e3f851a974e85be5a723204dfab0a870
Francisco Müller Amato <famato@infobytesec.com>
2023-08-21 13:19:36 +0000

    Update workspace.py

--- faraday_cli/shell/modules/workspace.py
+++ faraday_cli/shell/modules/workspace.py
@@ -400,7 +400,7 @@ class WorkspaceCommands(cmd2.CommandSet):
                     )
                 data.append(
                     [
-                        "\n".join(item) if type(item) == list else item
+                        "\n".join(item) if  if isinstance(item, list) else item
                         for item in workspace_data.values()
                     ]
                 )
